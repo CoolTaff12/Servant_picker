e9e190d5486c9e49918815fbca346856e2e612b5
Leo Taffazoli <cool_taff@yahoo.com>
2023-03-21 18:03:06 +0100

    Cleaner Code
    Found a way of excluding certain numbers and made the code cleaner - I still need to pick a way of updating the images.

--- main.py
+++ main.py
@@ -5,28 +5,32 @@ import requests
 from io import BytesIO
 import numpy as np
 
-#Create form root
+# Create form root
 root = tk.Tk()
 root.title("Servant Picker")
 root.geometry("800x400")
 
-#Pick random servant
-
+# Fill array with numbers which represents servants
 arr = np.arange(307)
+
+# Don't include these numbers  - 59, 83, 149, 151, 152, 168, 240, 333, 9944140
+b = np.array([0, 59, 83, 149, 151, 152, 168, 240, 333, 9944140])
+arr = np.setdiff1d(arr, b)
+
+# Shuffle server order
 np.random.shuffle(arr)
-arr = arr[arr > 0]
-# Don't include these servants - 59, 83, 149, 151, 152, 168, 240, 333, 9944140
 
-pack = 1
+slots = 1
 
-#Reset function
-def resetData():
+# Reset function
+def reset_data():
     print("Gerry")
 
-#Reset botton
-btn = tk.Button(root, text ="Resummon", command = resetData)
+# Reset button
+btn = tk.Button(root, text= "Re-summon", command= reset_data)
 btn.pack()
 
+# Pick random servant
 for x in arr:
 
     ran = str(x).zfill(3)
@@ -35,32 +39,31 @@ for x in arr:
     img_data = response.content
     img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
 
-    if pack == 6:
+    if slots == 6:
         my_img.append(img)
-        # Create pannel with added values
+        # Create panel with added values
         panel1 = tk.Label(root, width=64, height=64, image=my_img[0])
         panel2 = tk.Label(root, width=64, height=64, image=my_img[1])
         panel3 = tk.Label(root, width=64, height=64, image=my_img[2])
         panel4 = tk.Label(root, width=64, height=64, image=my_img[3])
         panel5 = tk.Label(root, width=64, height=64, image=my_img[4])
         panel6 = tk.Label(root, width=64, height=64, image=my_img[5])
-        panel1.pack(side="left", fill="both", expand="yes")
-        panel2.pack(side="left", fill="both", expand="yes")
-        panel3.pack(side="left", fill="both", expand="yes")
-        panel4.pack(side="left", fill="both", expand="yes")
-        panel5.pack(side="left", fill="both", expand="yes")
-        panel6.pack(side="left", fill="both", expand="yes")
+        panel1.pack(side="left", fill="both", expand=1)
+        panel2.pack(side="left", fill="both", expand=1)
+        panel3.pack(side="left", fill="both", expand=1)
+        panel4.pack(side="left", fill="both", expand=1)
+        panel5.pack(side="left", fill="both", expand=1)
+        panel6.pack(side="left", fill="both", expand=1)
         root.mainloop()
         # Open form
         username = input("Enter username:")
         print("Rest")
-        pack = 0
-    elif pack == 1:
+        slots = 0
+    elif slots == 1:
         my_img = [img]
     else:
         my_img.append(img)
-        print(pack)
+        print(ran)
 
-    pack = pack + 1
-
+    slots = slots + 1
 
