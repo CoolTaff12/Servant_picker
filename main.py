8704527e74f631e6b8a4074b4128acbbeda66b2d
Leo Taffazoli <cool_taff@yahoo.com>
2023-05-10 02:50:48 +0200

    SFX update
    Added a SFX folder for future sound effect implementation and a better special team set up

--- main.py
+++ main.py
@@ -59,26 +59,32 @@ exclude_these = [84, 150, 152, 153, 169, 241]
 latest_Servant_number = 308 + 2
 previous_servant = []
 
-music_cue = ""
-
-
 driver = webdriver.Chrome()
 # driver.maximize_window()
 driver.get("https://tiermaker.com/create/fgo-servant-tier-list-sorted-by-servant-id-1548189?ref=button")
 
-def SPECIAL_SUMMO_MUSIC(mp3_url):
-    x = vlc.MediaPlayer(mp3_url)
+def SPECIAL_SERVANT_TEAM(arguments):
+    team = {
+        1 : np.array([52, 38, 273, 2, 0, 0]),
+        2 : np.array([59, 203, 104, 2, 0, 0])
+    }
+    return team.get(arguments, np.array([0, 0, 0, 0, 0]))
+
+
+def SPECIAL_SUMMON_MUSIC(arguments):
+    file = {
+        1: "mPw6ecM33QzSi48Y.mp3",
+        2: "burunyaa.wav"
+    }
+    x = vlc.MediaPlayer('SFX/'+str(file.get(arguments)))
     x.play()
-    print("Runover")
+    print("Runover" + str(arguments))
 
 
 def SUMMON(servant_number, is_special):
 
-    if is_special:
-        # 53 , 39 , 274
-        confirmed_servant_data = Image.open("ryoma.png").resize((138, 150), Image.LANCZOS)
-        global music_cue
-        music_cue = "mPw6ecM33QzSi48Y.mp3"
+    if is_special and servant_number == 0:
+            confirmed_servant_data = Image.open("ryoma.png").resize((138, 150), Image.LANCZOS)
     else:
         wait = WebDriverWait(driver, 1)
         summoning_circle = wait.until(
@@ -101,7 +107,7 @@ def SUMMON(servant_number, is_special):
 def SUMMONING_CIRCLE():
     global new_servants
     global special_summon
-    throneOfHeroes = np.array([choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
+    throne_of_heroes = np.array([choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
                                choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
                                choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
                                choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
@@ -110,15 +116,18 @@ def SUMMONING_CIRCLE():
 
     new_servants = []
     special_summon = bool(randint(1, 6) == 3)
+    if special_summon:
+        random_twice = randint(1, 2)
+        throne_of_heroes = SPECIAL_SERVANT_TEAM(random_twice)
+        SPECIAL_SUMMON_MUSIC(random_twice)
 
-    for servant_order, servant_nr in enumerate(throneOfHeroes):
+    for servant_order, servant_nr in enumerate(throne_of_heroes):
         new_servants.append(SUMMON(servant_nr, special_summon))
-        canvas1.itemconfigure(previous_servant[servant_order],image=new_servants[servant_order])
+        canvas1.itemconfigure(previous_servant[servant_order], image=new_servants[servant_order])
 
-    if special_summon: SPECIAL_SUMMO_MUSIC(music_cue)
 
-# Randomize 6 servants numbers in throneOfHeroes
-throneOfHeroes = np.array([choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
+# Randomize 6 servants numbers in throne_of_heroes
+throne_of_heroes = np.array([choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
                            choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
                            choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
                            choice(list(set(range(2, latest_Servant_number)) - set(exclude_these))),
@@ -126,13 +135,13 @@ throneOfHeroes = np.array([choice(list(set(range(2, latest_Servant_number)) - se
                            choice(list(set(range(2, latest_Servant_number)) - set(exclude_these)))])
 
 servant_team = []
-for servant_order, servant_nr in enumerate(throneOfHeroes):
+for servant_order, servant_nr in enumerate(throne_of_heroes):
     servant_team.append(SUMMON(servant_nr, False))
     previous_servant.append(canvas1.create_image(15 + (138 * servant_order), 90, anchor="nw",
                                                  image=servant_team[servant_order]))
 
 
-btn = tk.Button(root, anchor="s", text="Re-summon", command=lambda:SUMMONING_CIRCLE(), background="#0095E5",
+btn = tk.Button(root, anchor="s", text="Re-summon", command=lambda: SUMMONING_CIRCLE(), background="#0095E5",
                 activebackground="#00A6FF", border=0)
 
 btn_canvas = canvas1.create_window(425, 30, anchor="s", window=btn)
