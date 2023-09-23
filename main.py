5b7616978c758b1bd739da6fd3e43654553b33cd
CoolTaff12 <cool_taff@yahoo.com>
2023-09-23 19:39:00 +0200

    Time reduction updates
    Trying my best to reduce the sudden loading time to get all servant images.
    
    gitignored added

--- main.py
+++ main.py
@@ -15,38 +15,51 @@ from selenium.webdriver.support.wait import WebDriverWait
 import vlc
 # In this case libvlc.dll will not be found. Make sure to install the 64bit-version
 # ----------------
+import time
+import asyncio
+from os import walk
+
+# ----------------
 
 
 # Runs in Python 3.11.
 # Package               version
 # certifi               2022.12.7 -> 2023.7.22
-# setuptools            60.2.0 -> 68.0.0
+# setuptools            60.2.0 -> 68.2.2
 # pip                   23.0.1 -> 23.1.2
 #                       21.3.1
-# Pillow                9.4.0 -> 10.0.0
+# Pillow                9.4.0 -> 10.0.1
 # idna                  3.4 -> (Same)
 # charset-normalizer    3.1.0 -> 3.2.0
 # requests              2.28.2 -> 2.31.0
-# urllib3               1.26.15 -> 2.0.4
-# numpy                 1.24.2 -> 1.25.2
-# wheel                 0.37.1 -> 0.41.1
+# urllib3               1.26.15 -> 2.0.5
+# numpy                 1.24.2 -> 1.26.0
+# wheel                 0.37.1 -> 0.41.2
 # python-vlc (vlc)      30.0.18122 -> (Same)
-# selenium              4.11.2
+# selenium              4.11.2 -> 4.12.0
+# asyncio               3.4.3
+
+
+# First, get all servant names that already is downloaded from the directory.
+alreadyRegisteredServants = next(walk('Servant_Images/'), (None, None, []))[2]  # [] if no file
+alreadyRegisteredServants = [fN.replace('.png','') for fN in alreadyRegisteredServants]
+
+print(alreadyRegisteredServants)
 
 # Create form root
 root = tk.Tk()
 root.title("Servant Picker")
-root.geometry("850x350")
+root.geometry("1290x350")
 colorCode = "#0276B4"
 root.configure(background=colorCode)
 
-# Create Canvas
-canvas1 = tk.Canvas(root, width=850, height=350, name="myCanvas")
+# Create Canvas             width=850, height=350, name="myCanvas"
+canvas1 = tk.Canvas(root, width=1290, height=350, name="myCanvas")
 
 canvas1.pack(fill="both", expand=True)
 
-# Add image file
-bg = Image.open("7waUcXA3cy8EK6Ct.png").resize((850, 350), Image.LANCZOS)
+# Add image file                                (850, 350), Image.LANCZOS
+bg = Image.open("7waUcXA3cy8EK6Ct.png").resize((1290, 350), Image.LANCZOS)
 bg = ImageTk.PhotoImage(bg)
 
 # Display image
@@ -73,6 +86,7 @@ driver.get("https://tiermaker.com/create/fgo-servant-tier-list-sorted-by-servant
 
 def ON_CLOSING():
     print("Goodbye")
+    loop.close()
     driver.close()
     root.destroy()
 
@@ -125,41 +139,31 @@ def SUMMON(servant_number, is_special):
     if is_special and servant_number == 0:
         confirmed_servant_data = Image.open("empty.png").resize((138, 150), Image.LANCZOS)
     else:
-        wait = WebDriverWait(driver, 1)
-        summoning_circle = wait.until(
-            ec.visibility_of_element_located((By.XPATH,
-                                              '//*[@id="create-image-carousel"]/div[' + str(servant_number) + ']')))
-        # Get image from div
-        summoning_servant = str(summoning_circle.get_attribute("style")).split('"')
-        addressing_servant_url = "https://tiermaker.com" + summoning_servant[1]
-        servants_response = requests.get(addressing_servant_url)
-        confirmed_servant_data = servants_response.content
+        if alreadyRegisteredServants.count(servant_number) > 0:
+            confirmed_servant_data = (Image.open("Servant_Images/" + servant_number + ".png")
+                                      .resize((138, 150), Image.LANCZOS))
+        else:
+            wait = WebDriverWait(driver, timeout=0.2)
+            summoning_circle = wait.until(
+                ec.visibility_of_element_located((By.XPATH,
+                                                  '//*[@id="create-image-carousel"]/div[' + str(servant_number) + ']')))
+            # Get image from div
+            summoning_servant = str(summoning_circle.get_attribute("style")).split('"')
+            addressing_servant_url = "https://tiermaker.com" + summoning_servant[1]
+            servants_response = requests.get(addressing_servant_url)
+            confirmed_servant_data = servants_response.content
 
-        # Adds the image and resizes it
-        confirmed_servant_data = Image.open(BytesIO(confirmed_servant_data)).resize((138, 150), Image.LANCZOS)
+            # Adds the image and resizes it
+            confirmed_servant_data = Image.open(BytesIO(confirmed_servant_data)).resize((138, 150), Image.LANCZOS)
+            # And downloads it!
+            confirmed_servant_data.save('Servant_Images/' + str(servant_number) + '.png')
 
     return ImageTk.PhotoImage(confirmed_servant_data)
 
 
-def SUMMONING_CIRCLE():
-    global new_servants
-    global special_summon
-    global reset_checks
-    re_summon = []
-    new_servants = []
-    no_replacement_request = bool(sum([psc.get() is True for psc in previous_servants_check]) not in range(1, 4))
-    allowed = [i for i, x in enumerate(previous_servants_check) if x.get()]
-
-    special_summon = bool(randint(1, 6) == 3)
-    random_special = 0
-    if special_summon and no_replacement_request:
-        random_special = randint(1, 4)
-        re_summon = SPECIAL_SERVANT_TEAM(random_special)
-    else:
-        re_summon = THRONE_OF_HEROES()
-
-    for servant_order, servant_nr in enumerate(re_summon):
-        new_servants.append(SUMMON(servant_nr, special_summon))
+async def CONTINUES_ASYNC(allowed, no_replacement_request, servant_order, servant_nr):
+    new_servants.append(SUMMON(servant_nr, special_summon))
+    try:
         if no_replacement_request and special_summon:
             canvas1.itemconfigure(previous_servant[servant_order], image=new_servants[servant_order])
 
@@ -176,6 +180,33 @@ def SUMMONING_CIRCLE():
 
         else:
             print("Oh oh! We skipped the rest of conditions at " + str(servant_order))
+    except:
+        print("Oh no! Error at servant_order: " + str(servant_order) + " & servant_nr: " + str(servant_nr))
+
+def SUMMONING_CIRCLE():
+    global new_servants
+    global special_summon
+    global reset_checks
+    new_servants = []
+    start = time.time()
+    summon_tasks = []
+    no_replacement_request = bool(sum([psc.get() is True for psc in previous_servants_check]) not in range(1, 4))
+    allowed = [i for i, x in enumerate(previous_servants_check) if x.get()]
+
+    special_summon = bool(randint(1, 6) == 3)
+    random_special = 0
+    if special_summon and no_replacement_request:
+        random_special = randint(1, 4)
+        re_summon = SPECIAL_SERVANT_TEAM(random_special)
+    else:
+        re_summon = THRONE_OF_HEROES()
+
+    for servant_order, servant_nr in enumerate(re_summon):
+        # Creating tasks here!
+        summon_tasks.append(loop.create_task(CONTINUES_ASYNC(
+            allowed, no_replacement_request, servant_order, servant_nr)))
+
+    loop.run_until_complete(asyncio.wait(summon_tasks))
 
     if reset_checks:
         DISABLE_OR_ENABLE_ALL(reset_checks)
@@ -188,24 +219,40 @@ def SUMMONING_CIRCLE():
 
     SET_ALL(False)
     print('click')
+    end = time.time()
+    print(f'Time: {end - start:.2f} sec')
 
 
-for servant_order, servant_nr in enumerate(THRONE_OF_HEROES()):
+async def START_ASYNC(servant_order, servant_nr):
     servant_team.append(SUMMON(servant_nr, False))
-    previous_servant.append(canvas1.create_image(15 + (138 * servant_order), 90, anchor="nw",
+    # 15 + (138 * servant_order), 90,
+    previous_servant.append(canvas1.create_image(215 + (138 * servant_order), 90, anchor="nw",
                                                  image=servant_team[servant_order]))
     psc = tk.BooleanVar()
     previous_servants_check.append(psc)
     check_boxes.append(canvas1.create_window(
-        85 + (138 * servant_order), 250,
+        # 85 + (138 * servant_order), 250,
+        285 + (138 * servant_order), 250,
         window=tk.Checkbutton(canvas1, background="black", activebackground="black",
                               variable=psc)))
 
+start = time.time()
+print('start')
+loop = asyncio.get_event_loop()
+tasks = []
+
+for servant_order, servant_nr in enumerate(THRONE_OF_HEROES()):
+    tasks.append(loop.create_task(START_ASYNC(servant_order, servant_nr)))
+
+
+loop.run_until_complete(asyncio.wait(tasks))
+end = time.time()
+print(f'Time: {end - start:.2f} sec')
 
 btn = tk.Button(root, anchor="s", text="Re-summon", command=lambda: SUMMONING_CIRCLE(), background="#0095E5",
                 activebackground="#00A6FF", border=0)
-
-btn_canvas = canvas1.create_window(425, 30, anchor="s", window=btn)
+# 425, 30, anchor="s", window=btn
+btn_canvas = canvas1.create_window(625, 30, anchor="s", window=btn)
 
 
 driver.minimize_window()
