3e348102de059e6ce336692892529855944a3316
CoolTaff12 <cool_taff@yahoo.com>
2024-05-31 22:00:28 +0200

    31th May 2024 Update
    Removed VLC as its useless, updated the packages, new link with newer servants and a new group audio.

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 # ----------------
-# vlc name is python-vlc if you want to install it
-import vlc
-# In this case libvlc.dll will not be found. Make sure to install the 64bit-version
+# pygame is for the audio only
+import pygame
 # ----------------
 import time
 import asyncio
@@ -25,25 +24,29 @@ from os import walk
 # Runs in Python 3.12.
 # Package               version
 # certifi               2022.12.7 -> 2024.2.2
-# setuptools            60.2.0 -> 69.0.3
-# pip                   23.0.1 -> 23.3.2
+# setuptools            60.2.0 -> 70.0.0
+# pip                   23.0.1 -> 24.1b1
 #                       21.3.1
-# Pillow                9.4.0 -> 10.2.0
-# idna                  3.4 -> 3.6
+# Pillow                9.4.0 -> 10.3.0
+# idna                  3.4 -> 3.7
 # charset-normalizer    3.1.0 -> 3.3.2
-# requests              2.28.2 -> 2.31.0
-# urllib3               1.26.15 -> 2.2.0
-# numpy                 1.24.2 -> 1.26.3
-# wheel                 0.37.1 -> 0.42.0
+# requests              2.28.2 -> 2.32.3
+# urllib3               1.26.15 -> 2.2.1
+# numpy                 1.24.2 -> 2.0.0rc2
+# wheel                 0.37.1 -> 0.43.0
 # python-vlc (vlc)      30.0.18122 -> 3.0.20123
-# selenium              4.11.2 -> 4.17.2
+# selenium              4.11.2 -> 4.21.0
 # asyncio               3.4.3
+# pygame                2.5.2
 
 # First, get all servant names that already is downloaded from the directory.
 alreadyRegisteredServants = next(walk('Servant_Images/'), (None, None, []))[2]  # [] if no file
 alreadyRegisteredServants = [fN.replace('.png', '') for fN in alreadyRegisteredServants]
 
 closetFilledWithMystic: str = next(walk('Mystic_Codec/'), (None, None, []))[2]  # [] if no file
+
+pygame.init()
+pygame.mixer.init()
 
 # print(alreadyRegisteredServants)
 
@@ -94,8 +97,9 @@ canvas1.create_oval(circle_x_positions[0], circle_y_positions[0],
 # canvas1.create_text(200, 250, text="Welcome")
 
 # Exclude these servants: 150 152 153 169 241 334
-exclude_these = [84, 150, 152, 153, 169, 241, 334]
-latest_Servant_number = 336 + 2 #Bazett
+exclude_these = [68, 70, 86, 135, 137, 155, 157, 158, 174, 221, 222, 248, 249, 250, 261, 262, 281, 282, 292, 294, 338,
+                 339, 351, 254, 355, 357, 360, 362, 369, 371]
+latest_Servant_number = 373  # Zhang Jue (372)
 servant_team = []
 previous_servant = []
 previous_servants_check = []
@@ -111,7 +115,9 @@ print(webCheck)
 
 if webCheck:
     driver = webdriver.Chrome()
-    driver.get("https://tiermaker.com/create/fgo-servant-tier-list-sorted-by-servant-id-1548189?ref=button")
+    # Old
+    # https://tiermaker.com/create/fgo-tier-list-but-actually-with-all-the-servants-55890?ref=button
+    driver.get("https://tiermaker.com/create/fgo-all-servants-20-always-updated-1024967?ref=button")
 
 
 def ON_CLOSING():
@@ -134,7 +140,6 @@ def DISABLE_OR_ENABLE_ALL(function_mode):
 
 
 def THRONE_OF_HEROES():
-
     # Randomize 6 servants numbers in throne_of_heroes
     number_list = []
     slots = 6
@@ -143,7 +148,7 @@ def THRONE_OF_HEROES():
         slots = randint(3, 5)
         number_list = np.random.choice(list(set(range(2, latest_Servant_number)) - set(exclude_these)), slots)
         while len(number_list) < 6:
-            number_list = np.append(number_list,[0])
+            number_list = np.append(number_list, [0])
     else:
         number_list = np.random.choice(list(set(range(2, latest_Servant_number)) - set(exclude_these)), slots)
     return number_list
@@ -152,36 +157,42 @@ def THRONE_OF_HEROES():
 def SPECIAL_SERVANT_TEAM(arguments):
     team = {
         # Umu
-        1: np.array([6, 91, 176, 0, 0, 0]),
+        1: np.array([7, 94, 181, 0, 0, 0]),
         # Feline
-        2: np.array([59, 203, 104, 149, 2, 0]),
+        2: np.array([60, 208, 107, 154, choice([2, 3]), 0]),
         # Suicide Run
-        3: np.array([choice([252, 204]), 17, 259, 108, 295, 274]),
+        3: np.array([choice([263, 209]), 18, 270, 111, 310, 287]),
         # ONLY DIO
-        4: np.array([36, 119, 34, 2, 0, 0]),
+        4: np.array([37, 122, 35, choice([2, 3]), 0, 0]),
         # Only Elisabeth
-        5: np.array([19, 62, 139, 191, 192, 47]),
+        5: np.array([20, 63, 144, 344, 196, 197]),
         # Cooking team
-        6: np.array([12, 27, 59, 331, 235, 0])
+        6: np.array([13, 28, 60, 348, 242, 0])
     }
     return team.get(arguments, np.array([0, 0, 0, 0, 0]))
 
 
 def SPECIAL_SUMMON_MUSIC(arguments):
     file = {
-        1: ["S005_SelectAttack1.mp3", 10.1],
-        2: ["burunyaa.wav", 10.1],
-        3: ["suicide_eng.wav", 10.1],
-        4: ["Laughs_in_muda.mp3", 10.1],
-        5: ["S018_NP2.mp3", 10.1]
+        1: ["S005_SelectAttack1.mp3", 10],
+        2: ["burunyaa.wav", 10],
+        3: ["suicide_eng.wav", 10],
+        4: ["Laughs_in_muda.mp3", 10],
+        5: ["S018_NP2.mp3", 10],
+        6: ["Hells_Kitchen_Dramatic_Sound.mp3", 10]
     }
-    play_the_fanfare = vlc.MediaPlayer('SFX/'+str(file.get(arguments)[0]))
-    play_the_fanfare.play()
+
+    # play_the_fanfare = vlc.MediaPlayer('SFX/' + str(file.get(arguments)[0]))
+    sound = pygame.mixer.Sound('SFX/' + str(file.get(arguments)[0]))
+    sound.set_volume(0.9)  # Now plays at 90% of full volume.
+    sound.play()
+    # print(file.get(arguments)[1])
+    # play_the_fanfare.audio_set_volume(10)
+    # play_the_fanfare.play()
     print("Team " + str(arguments))
 
 
 def SUMMON(servant_number, is_special):
-
     if is_special and servant_number == 0:
         confirmed_servant_data = Image.open("empty.png").resize((138, 150), Image.LANCZOS)
     elif is_special is False and servant_number == 0:
@@ -194,10 +205,12 @@ def SUMMON(servant_number, is_special):
             wait = WebDriverWait(driver, timeout=0.2)
             summoning_circle = wait.until(
                 ec.presence_of_element_located((By.XPATH,
-                                                  '//*[@id="create-image-carousel"]/div[' + str(servant_number) + ']')))
+                                                '//*[@id="create-image-carousel"]/div[' + str(servant_number) + ']')))
             # Get image from div
             summoning_servant = str(summoning_circle.get_attribute("style")).split('"')
-            addressing_servant_url = "https://tiermaker.com" + summoning_servant[1]
+            addressing_servant_url = summoning_servant[1]
+            # print(servant_number)
+            # print(addressing_servant_url)
             servants_response = requests.get(addressing_servant_url)
             confirmed_servant_data = servants_response.content
 
@@ -208,6 +221,7 @@ def SUMMON(servant_number, is_special):
             alreadyRegisteredServants.append(servant_number)
 
     return ImageTk.PhotoImage(confirmed_servant_data)
+
 
 async def CONTINUES_ASYNC(allowed, no_replacement_request, servant_order, servant_nr):
     new_servants.append(SUMMON(servant_nr, special_summon))
@@ -238,14 +252,21 @@ def SUMMONING_CIRCLE():
     no_replacement_request = bool(sum([psc.get() is True for psc in previous_servants_check]) not in range(1, 4))
     allowed = [i for i, x in enumerate(previous_servants_check) if x.get()]
 
+    # Chance of special summon
     special_summon = bool(randint(1, 6) == 3)
+    # Force special summon  (for testing)
+    # special_summon = True
     random_special = 0
     if special_summon and no_replacement_request:
+        # Always random
         random_special = randint(1, 6)
+        # Not random (for testing)
+        # random_special = 2
         re_summon = SPECIAL_SERVANT_TEAM(random_special)
     else:
         re_summon = THRONE_OF_HEROES()
-        newClothes = Image.open("Mystic_Codec/" + closetFilledWithMystic[randint(0, 15)]).resize((70, 73), Image.LANCZOS)
+        newClothes = Image.open("Mystic_Codec/" + closetFilledWithMystic[randint(0, 15)]).resize((70, 73),
+                                                                                                 Image.LANCZOS)
         newClothes = ImageTk.PhotoImage(newClothes)
         canvas1.itemconfigure(mysticCodec, image=newClothes)
 
@@ -284,15 +305,14 @@ async def START_ASYNC(servant_order, servant_nr):
         window=tk.Checkbutton(canvas1, background="black", activebackground="black",
                               variable=psc)))
 
+
 start = time.time()
 print('start')
 loop = asyncio.new_event_loop()
 tasks = []
 
-
 for servant_order, servant_nr in enumerate(THRONE_OF_HEROES()):
     tasks.append(loop.create_task(START_ASYNC(servant_order, servant_nr)))
-
 
 loop.run_until_complete(asyncio.wait(tasks))
 end = time.time()
